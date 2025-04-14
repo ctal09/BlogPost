65f7a68878ca15ff6321f605be88dd5360afea5b
ctal09 <144703146+ctal09@users.noreply.github.com>
2025-04-14 09:44:33 +0545

    CreatePost functionality added but routing left

--- Store/Post-list-Store.jsx
+++ Store/Post-list-Store.jsx
@@ -1,64 +1,100 @@
 import { Children, createContext, useReducer } from "react";
 import Postlist from "../src/Components/Postlist";
 
-
- export const PostList = createContext({
-    postList: [],
-    addpost: ()=>{},
-    deletepost:()=>{},
+export const PostList = createContext({
+  postList: [],
+  addpost: () => {},
+  deletepost: () => {},
 });
 
-const postListreducer = (currPostList, action) =>{
-    let newpostlist = currPostList;
-    if(action.type==='Delete_post'){
-        newpostlist= currPostList.filter(post=>post.id!=action.payload.postID);
-    }
-    return newpostlist;
-}
+const postListreducer = (currPostList, action) => {
+  let newpostlist = currPostList;
+  if (action.type === "Delete_post") {
+    newpostlist = currPostList.filter(
+      (post) => post.id != action.payload.postID
+    );
+  } else if (action.type === "Add_post") {
+    newpostlist = [action.payload, ...currPostList];
+  }
+  return newpostlist;
+};
 
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListreducer,
+    Default_Postlist
+  );
 
+  const addpost = (UserId, title, body, reactions, tag) => {
+    dispatchPostList({
+      type: "Add_post",
+      payload: {
+        id: Date.now,
+        Post_Title: title,
+        body: body,
+        no_of_reaction: reactions,
+        userID: UserId,
+        tags: tag,
+      },
+    });
+  };
 
+  const deletepost = (postID) => {
+    dispatchPostList({
+      type: "Delete_post",
+      payload: {
+        postID,
+      },
+    });
+  };
 
-const PostListProvider =({ children })=>{
+  return (
+    <PostList.Provider value={{ postList, addpost, deletepost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
 
-        const [postList,dispatchPostList]= useReducer(postListreducer, Default_Postlist);
-
-
-        const addpost=(UserId,title,body,reactions,tag)=>{
-            console.log(`${UserId},${title},${body},${reactions},${tag}`)
-
-        };
-        
-        
-        const deletepost = (postID)=>{
-            dispatchPostList({
-                type: 'Delete_post',
-                payload: {
-                    postID,
-                },
-            });
-        
-        };
-
-
-     return <PostList.Provider value={{postList, addpost, deletepost}}>{children}</PostList.Provider>
-}
-
-
-const Default_Postlist = [{
-    id:'1',
-    Post_Title:'Go To Pokhara',
-    body:'Hi Friends, I am going to Pokhara for my vacation. I Hope I will enjoy it a lot. Peace Out',
-    no_of_reaction:'2',
-    userID:'user-76',
-    tags:["vacation", "Pokhara"],
-},
-{
-    id:'2',
-    Post_Title:'Go To Chitwan',
-    body:'Hi Friends, I am going to Chitwan for my vacation. I Hope I will enjoy it a lot. Peace Out',
-    no_of_reaction:'27',
-    userID:'user-71',
-    tags:["vacation", "Chitwan"],
-}]
+const Default_Postlist = [
+  {
+    id: "1",
+    Post_Title: "Go To Pokhara",
+    body: "Hi Friends, I am going to Pokhara for my vacation. I Hope I will enjoy it a lot. Peace Out",
+    no_of_reaction: "2",
+    userID: "user-76",
+    tags: ["vacation", "Pokhara"],
+  },
+  {
+    id: "2",
+    Post_Title: "Go To Chitwan",
+    body: "Hi Friends, I am going to Chitwan for my vacation. I Hope I will enjoy it a lot. Peace Out",
+    no_of_reaction: "27",
+    userID: "user-71",
+    tags: ["vacation", "Chitwan"],
+  },
+  {
+    id: "2354",
+    Post_Title: "How Your ",
+    body: "Hi Friends,  Peace Out",
+    no_of_reaction: "276",
+    userID: "user-71",
+    tags: ["Peace", "Chill"],
+  },
+  {
+    id: "223354",
+    Post_Title: "How adsjajdYour ",
+    body: "Hi Friends, adslajdla Peace Out",
+    no_of_reaction: "2476",
+    userID: "user-7ss1",
+    tags: ["Peace", "Chalkjdlaill"],
+  },
+  {
+    id: "22eee3354",
+    Post_Title: "How adsdfbsbfs,sjajdYour ",
+    body: "Hi Frsdfjskdfiends, adslajdla Peace Out",
+    no_of_reaction: "23e34476",
+    userID: "usesdfbskdfhsfr-7ss1",
+    tags: ["sfakjhfjls", "Chalkjdlaill"],
+  },
+];
 export default PostListProvider;
--- src/Components/createpost.jsx
+++ src/Components/createpost.jsx
@@ -1,56 +1,94 @@
 import { useContext, useRef } from "react";
 import { PostList } from "../../Store/Post-list-Store";
 
-const CreatePost =()=>{
-const {addpost}=useContext(PostList)
+const CreatePost = () => {
+  const { addpost } = useContext(PostList);
 
-const UserIdele=useRef();
-const titleele=useRef();
-const bodyele=useRef();
-const reactionsele=useRef();
-const tagsele=useRef();
+  const UserIdele = useRef();
+  const titleele = useRef();
+  const bodyele = useRef();
+  const reactionsele = useRef();
+  const tagsele = useRef();
 
-const handlesubmit=(event)=>{
-  event.preventDefault();
-  const UserId= UserIdele.current.value;
-  const title=titleele.current.value;
-  const body=bodyele.current.value;
-  const reactions=reactionsele.current.value;
-  const tags=tagsele.current.value.split(" ");
-  
-  addpost(UserId,title,body,reactions,tags);
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    const UserId = UserIdele.current.value;
+    const title = titleele.current.value;
+    const body = bodyele.current.value;
+    const reactions = reactionsele.current.value;
+    const tags = tagsele.current.value.split(" ");
+
+    addpost(UserId, title, body, reactions, tags);
+  };
+
+  return (
+    <form className="Createpost" onSubmit={handlesubmit}>
+      <div className="mb-3">
+        <label htmlFor="UserId" className="form-label">
+          Enter your User ID here...
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="UserId"
+          placeholder="Enter Your User Id..."
+          ref={UserIdele}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="How are you feeling today...."
+          ref={titleele}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="Tell us more about it..... "
+          ref={bodyele}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of Reactions on this post?
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="reactions"
+          placeholder="How many people reacted to your post?"
+          ref={reactionsele}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter tags
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="tags"
+          placeholder="You can enter multiple tags...."
+          ref={tagsele}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 };
-
-
-
-
-return <form className="Createpost" onSubmit={handlesubmit}>
-
-  <div className="mb-3">
-    <label htmlFor="UserId" className="form-label">Enter your User ID here...</label>
-    <input type="text" className="form-control" id="UserId" placeholder="Enter Your User Id..." ref={UserIdele}/>
-   </div>
-
-  <div className="mb-3">
-    <label htmlFor="title" className="form-label">Post Title</label>
-    <input type="text" className="form-control" id="title" placeholder="How are you feeling today...." ref={titleele}/>
-   </div>
-   <div className="mb-3">
-    <label htmlFor="body" className="form-label">Post Content</label>
-    <textarea type="text" className="form-control" id="title" placeholder="Tell us more about it..... " ref={bodyele}/>
-   </div>
-   <div className="mb-3">
-    <label htmlFor="reactions" className="form-label">Number of Reactions on this post?</label>
-    <input type="text" className="form-control" id="reactions" placeholder="How many people reacted to your post?" ref={reactionsele}/>
-   </div>
-   <div className="mb-3">
-    <label htmlFor="tags" className="form-label">Enter tags</label>
-    <input type="text" className="form-control" id="tags" placeholder="You can enter multiple tags...." ref={tagsele}/>
-   </div>
-  
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-
-
-}
 export default CreatePost;
